fef8b3a0836eccad007bebbcba5f3abe397299fb
Andrew <39838470+compbiolover@users.noreply.github.com>
2022-03-04 15:14:18 -0500

    More comments on the risk score function

--- Code/risk_score_calculator.R
+++ Code/risk_score_calculator.R
@@ -31,11 +31,13 @@ risk_score_calculator <- function(my.file=active_coefs.csv,
   source("Code/km_plotter.R")
   
   #List to store the return objects in----
-  survival_return <- vector(mode = "list", length = 2)
+  survival_return <- list()
   
   #Read in the data----
   my_file <- read.csv(my.file)
+  #Subsetting to just he columns we need
   my_file <- my_file[,2:3]
+  #Renaming them nicer names
   colnames(my_file)[c(1,2)] <- c("gene","coef")
   
   
@@ -49,28 +51,32 @@ risk_score_calculator <- function(my.file=active_coefs.csv,
     #Making a vector of gene signs based on coefficient values
     gene_sign <- ifelse(my_file$coef>0, 1,-1)
     print(length(gene_sign))
-    #Doing matrix multiplication of risk_df matrix by the diagonal of the
-    #gene_sign vector
+    #Doing matrix multiplication of risk_df matrix by the gene_sign vector
     risk_df <- risk_df%*%diag(gene_sign)
     risk_df <- as.data.frame(risk_df)
-    View(risk_df)
+    #View(risk_df)
     #Giving the column names nicer names
     colnames(risk_df) <- my_file$gene
+    
     #Making a data frame of using the apply function on the columns of the risk
     # data frame and getting their median values
     gene_info <- data.frame(med_expression=apply(risk_df, 2, median))
+    
     #Transposing it
     gene_info <- t(gene_info)
     gene_info <- as.data.frame(gene_info)
     my_genes <- colnames(risk_df[1:length(colnames(risk_df))])
+    
     #Getting the censoring information and survival time from the cox data frame
     risk_df$vital.status <- cox.df$vital.status
     risk_df$time <- cox.df$days.to.last.follow.up
+    
     counter <- 1
     my_converted_scores <- list()
     total_risk_length <- length(colnames(risk_df))
     risk_length <- total_risk_length-2
-    #Looping through all of the genes and comparing their express
+    
+    #Looping through all of the genes and comparing their expression 
     for(x in my_genes){
       current_risk <- risk_converter(my.name = my_genes[counter],
                                      my.data = risk_df[1:risk_length],
@@ -87,33 +93,52 @@ risk_score_calculator <- function(my.file=active_coefs.csv,
       counter <- counter + 1
     }
     
-    #Removing an unnecessary column 
+    #Removing the unnecessary "my_df" column from our binding of the different
+    #vectors from the previous for loop
     converted_df[,"my_df"] <- NULL
-    #Setting the risk score data frame's rownames
+    
+    #Setting the risk score data frame's row names
     rownames(converted_df) <- rownames(cox.df)
+    
+    #Adding the survival time and censoring status 
+    #values from the cox data frame
     converted_df$vital.status <- cox.df$vital.status
     converted_df$time <- cox.df$days.to.last.follow.up
+    
+    #Ensuring all columns in the risk data frame are of type 'numeric'
     converted_df <- apply(converted_df, 2, as.numeric)
+    
+    #Summing up each patient's risk by summing across all of the different
+    #genes and their respective values for that particular patient
     patient_risks <- rowSums(x=converted_df[,1:risk_length])
     converted_df <- as.data.frame(converted_df)
     converted_df$risk <- patient_risks
+    
+    #Ensuring all of our columns are of type 'numeric'
     converted_df <- apply(converted_df, 2, as.numeric)
+    
+    #Taking the median of the absolute value of all columns in the risk data
+    #frame because we don't want the sign of the calculation to affect the 
+    #magnitude
     median_risk <- median(abs(converted_df[,1:risk_length]))
     converted_df <- as.data.frame(converted_df)
-    #Binirizing the risk score into high and low groups
-    converted_df$risk <- ifelse(converted_df$risk>median_risk, "high", "low")
-    #Fitting our inputs with survfit and seeing how well our risk score
-    #stratifies the high versus low risk groups
-    km_fit <- survfit(Surv(time, vital.status) ~ risk, data = converted_df)
-    #Plotting the outcome of the fit
-    finished_plot <- km_plotter(km.fit = km_fit, data.source = converted_df,
-                                p.value = TRUE,
-                                plot.title = my.title)
-    #Viewing the finished plot
-    finished_plot
     
-    #Adding the finished plot to our return list
-    survival_return[["KM Plot"]] <- finished_plot
+    #Binarizing the risk score into high and low groups
+    converted_df$risk <- ifelse(converted_df$risk>median_risk, "high", "low")
+    
+    #Fitting our inputs with the survfit function and seeing how well our risk
+    #score stratifies the high versus low risk groups
+    km_fit <- survfit(Surv(time, vital.status) ~ risk, data = converted_df)
+    
+    #Plotting the outcome of the fit
+    # finished_plot <- km_plotter(km.fit = km_fit, data.source = converted_df,
+    #                             p.value = TRUE,
+    #                             plot.title = my.title)
+    #Viewing the finished plot
+    # finished_plot
+    # 
+    # #Adding the finished plot to our return list
+    # survival_return[["KM Plot"]] <- finished_plot
   }
   
   # #Only for if there is tumor data included----
@@ -279,6 +304,7 @@ risk_score_calculator <- function(my.file=active_coefs.csv,
   
   #Attaching the risk scoring data frame
   survival_return[["Survival DF"]] <- converted_df
+  
   #Returning our list of finished objects
   return(survival_return)
 }
