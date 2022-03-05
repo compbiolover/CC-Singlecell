cdda4ad3b0bf2ae640febff9998b3341c37a0491
Andrew <39838470+compbiolover@users.noreply.github.com>
2022-03-05 01:46:17 -0500

    Improvments to saving of figures and setting the root of pt

--- Code/cell_dataset_builder.R
+++ Code/cell_dataset_builder.R
@@ -1,16 +1,25 @@
 #Name: cell_dataset_builder.R----
 #Author: Andrew Willems <awillems@vols.utk.edu>
-#Purpose: Efficiently build cell dataset objects
+#Purpose: Efficiently build cell data set objects
 #and get pseudotime information. 
 
-#cell_dataset_builder----
 cell_dataset_builder <- function(vim.genes=vim,
                                  cell.data=denoised_sc,
                                  cell.meta=gene_metadata,
                                  graph.root=1.5,
                                  show.traj=TRUE,
-                                 point.size=2.5){
+                                 point.size=2.5,
+                                 my.monocle.graph = "~/Desktop/pseudotime_graph.svg",
+                                 my.monocle.graph.genes = "~/Desktop/genes_graph.svg",
+                                 my.moncole.plot.type = "svg",
+                                 my.monocle.plot.dpi = 300,
+                                 my.monocle.width = 15,
+                                 my.monocle.height = 15,
+                                 my.monocle.unit = "cm",
+                                 my.cds.filename = "~/Desktop/cds_output.rds",
+                                 my.pt.data.filename = "~/Desktop/pt_data.csv"){
   #Required packages----
+  require(ggplot2)
   require(monocle3)
   
   #Return list----
@@ -20,27 +29,27 @@ cell_dataset_builder <- function(vim.genes=vim,
   vim_genes <- vim.genes
   cell_data_set <- new_cell_data_set(cell.data, gene_metadata = cell.meta)
   cds <- preprocess_cds(cell_data_set, num_dim=100, method="PCA")
-  cds <- reduce_dimension(cds)
-  plot_cells(cds)
-  cds <- cluster_cells(cds)
-  plot_cells(cds, color_cells_by="partition")
-  vim_plot <- plot_cells(cds, genes=vim_genes, cell_size=0.50)
+  cds <- reduce_dimension(cds, reduction_method = "UMAP")
+  cds <- cluster_cells(cds, random_seed = 123)
+  partition_plot <-plot_cells(cds, color_cells_by="partition")
+  vim_plot <- plot_cells(cds, genes=vim_genes, cell_size=point.size)
   cds <- learn_graph(cds)
-  cds <- order_cells(cds)
-  pt_graph <-plot_cells(cds      = cds,
-             color_cells_by      = "pseudotime",
-             label_cell_groups   = FALSE,
-             label_leaves        = FALSE,
-             label_branch_points = FALSE,
-             graph_label_size    = graph.root,
-             show_trajectory_graph = show.traj,
-             cell_size = point.size)
+  cds <- order_cells(cds, root_pr_nodes = "Y_9")
+  pt_graph <-plot_cells(cds         = cds,
+             color_cells_by         = "pseudotime",
+             label_cell_groups      = FALSE,
+             label_leaves           = FALSE,
+             label_branch_points    = TRUE,
+             label_principal_points = TRUE,
+             graph_label_size       = graph.root,
+             show_trajectory_graph  = show.traj,
+             cell_size              = point.size)
   
   
   #Integrating the new pseudotime data into the count matrix.----
   #Here I extract just the pseudotime calculations from the cds
-  #object to a use for the swithde() calculation later. 
-  #The output of this code is a dataframe that has 2 columns (pseudotime
+  #object to a use for the swithde function calculation later. 
+  #The output of this code is a data frame that has 2 columns (pseudotime
   #value and sample name[cell name]) and 375 rows
   #(cells with their associated pseudotime).
   pseudotime_data <- cds@principal_graph_aux@listData[["UMAP"]][["pseudotime"]]
@@ -48,6 +57,27 @@ cell_dataset_builder <- function(vim.genes=vim,
   colnames(pseudotime_data) <- "Pseudotime"
   pseudotime_contents <- rownames(pseudotime_data)
   pseudotime_data$Samples <- pseudotime_contents
+  
+  #Saving the  various plots to .svg or other specified format
+  #with particular parameters
+  
+  #The pseudotime graph
+  ggsave(filename = my.monocle.graph,
+         plot     = print(pt_graph, newpage = FALSE),
+         device   = my.moncole.plot.type, dpi=my.monocle.plot.dpi,
+         width    = my.monocle.width, height = my.monocle.height,
+         units    = my.monocle.unit)
+  
+  #The gene expression graph
+  ggsave(filename = my.monocle.graph.genes,
+         plot     = print(vim_plot, newpage = FALSE),
+         device   = my.moncole.plot.type, dpi=my.monocle.plot.dpi,
+         width    = my.monocle.width, height = my.monocle.height,
+         units    = my.monocle.unit)
+  
+  #Saving the relevant R objects and data frames
+  saveRDS(cds, my.cds.filename)
+  write.csv(pseudotime_data, my.pt.data.filename)
   
   #Returning our objects----
   cds_return_list[["CDS"]] <- cds
